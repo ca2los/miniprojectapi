c64d0b84b1eb5133fc8a0d1d5964bd55e5c4d77e
ca2los <{ID}+{username}@users.noreply.github.com>
2021-07-27 00:25:14 -0500

    Changes to calculate the remaining days and total earnings.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,32 +13,30 @@
     function submit_form(event) {
         event.preventDefault();
 
+        // JQ Input
         let pro_name = $('input[id="project-name-input"]');
         let pro_type = $('#project-type-input');
         let pro_hour = $('input[id="hourly-rate-input"]');
         let pro_date = $('input[id="datepicker"]');
-        //let pro_today  = String(moment().subtract(10, 'days').calendar());
-        //let pro_remain = pro_date - pro_actd;
 
-        console.log('Name: ' + pro_name.val());
-        console.log('Type: ' + pro_type.val());
-        console.log('Hour: ' + pro_hour.val());
-        console.log('Date: ' + pro_date.val());
-        //console.log('Due:  ' + pro_dued);
+        // JQ Output
+        let due_day = moment(pro_date.val()).format('D');
+        let todayis = moment().format("D");
+        let earning = pro_hour.val() * (due_day - todayis);
 
         add_form.append('<tr class="form-list">' +
                         '<th scope="col">' + pro_name.val() + '</th>' +
                         '<th scope="col">' + pro_type.val() + '</th>' +
                         '<th scope="col">' + pro_hour.val() + '</th>' +
                         '<th scope="col">' + pro_date.val() + '</th>' +
-                        '<th scope="col">' + "PENDING" + '</th>' +
-                        '<th scope="col">' + '<button class="remove_item">Remove</button>' +
+                        '<th scope="col">' + (due_day - todayis) + '</th>' +
+                        '<th scope="col">' + earning + '</th>' +
+                        '<th scope="col">' + '<button class="remove_item">❌</button>' +
                         '</th>');
 
         function delete_line(event) {
             $(event.target).parent().parent().remove();
         }
         add_form.on('click','.remove_item', delete_line);
-
     }
     pro_form.on('submit', submit_form);
